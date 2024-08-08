ed3810bf0f3a8e6146463d93007ce8a7e7de0a62
João Pedro <jopedroribeiro9@gmail.com>
2024-08-08 17:07:00 -0300

    teste2

---
+++ frontend/api/produtos.js
@@ -0,0 +1,29 @@
+const https = require('https');
+
+export default function handler(req, res) {
+  const path = req.url.replace('/api/produtos', '');
+  const options = {
+    hostname: 'produtos-ambiente-env-1.eba-njrz2a2f.sa-east-1.elasticbeanstalk.com',
+    path: `/api/produtos${path}`,
+    method: 'GET'
+  };
+
+  const request = https.request(options, response => {
+    let data = '';
+
+    response.on('data', chunk => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      res.status(200).json(JSON.parse(data));
+    });
+  });
+
+  request.on('error', error => {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  });
+
+  request.end();
+}
